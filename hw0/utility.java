fea427c93c273d4832f9c98ca36b5f03c46976b4
Tyler Rathkamp <tyrathkamp@berkeley.edu>
2020-01-28 20:39:12 -0800

    threeSum completed

--- hw0/utility.java
+++ hw0/utility.java
@@ -14,5 +14,19 @@ public class utility {
     }
     public static void main(String[] args){
         System.out.println(threeSum(new int[]{-6, 2, 5}));
+        System.out.println(threeSumDistinct(new int[]{-6, 2, 4}));
+    }
+    public static boolean threeSumDistinct(int[] array){
+        for (int first = 0; first < array.length; first++) {
+            for (int second = first + 1; second < array.length; second++) {
+                for (int i = second + 1; i < array.length; i++) {
+                    if (array[first] + array[second] + array[i] == 0) {
+                        return true;
+                    }
+
+                }
+            }
+        }
+        return false;
     }
 }
