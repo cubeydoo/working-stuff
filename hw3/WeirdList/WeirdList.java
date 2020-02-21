0cb5e9d81c7fd211a8c3045d318a364ef8343c31
Tyler Rathkamp <tyrathkamp@berkeley.edu>
2020-02-20 20:45:50 -0800

    weird

--- hw3/WeirdList/WeirdList.java
+++ hw3/WeirdList/WeirdList.java
@@ -1,20 +1,22 @@
 /** A WeirdList holds a sequence of integers.
- * @author your name here
+ * @author Tyler Rathkamp
  */
 public class WeirdList {
+    private int _head;
+    private WeirdList _tail;
     /** The empty sequence of integers. */
-    public static final WeirdList EMPTY =
-        null;  // TODO: REPLACE THIS LINE
+    public static final WeirdList EMPTY = new WeirdListEmpty.WeirdL;
 
     /** A new WeirdList whose head is HEAD and tail is TAIL. */
     public WeirdList(int head, WeirdList tail) { 
-        /* TODO: FILL IN */ 
+        _head = head;
+        _tail = tail;
     }
 
     /** Returns the number of elements in the sequence that
      *  starts with THIS. */
     public int length() {
-        return 0;  // TODO: REPLACE THIS LINE
+        return 1 + _tail.length();
     }
 
     /** Return a string containing my contents as a sequence of numerals
@@ -22,49 +24,55 @@ public class WeirdList {
      *  5, 4, and 2, this returns " 5 4 2". */
     @Override
     public String toString() {
-        return ""; // TODO: REPLACE THIS LINE
+        return String.valueOf(_head) + " " + _tail.toString();
     }
 
     /** Part 3b: Apply FUNC.apply to every element of THIS WeirdList in
      *  sequence, and return a WeirdList of the resulting values. */
     public WeirdList map(IntUnaryFunction func) {
-        return null;  // TODO: REPLACE THIS LINE
+        return new WeirdList(func.apply(_head), _tail.map(func));
     }
+    
 
-    /*
-     * You should not add any methods to WeirdList, but you will need
-     * to add private fields (e.g. head).
 
-     * But that's not all!
 
-     * You will need to create at least one additional class for WeirdList
-     * to work. This is because you are forbidden to use any of the
-     * following in ANY of the code for HW3:
-     *       if, switch, while, for, do, try, or the ?: operator.
 
-     * If you'd like an obtuse hint, scroll to the very bottom of this
-     * file.
 
-     * You can create this hypothetical class (or classes) in separate
-     * files like you usually do, or if you're feeling bold you can
-     * actually stick them INSIDE of this class. Yes, nested classes
-     * are a thing in Java.
+        /*
+         * You should not add any methods to WeirdList, but you will need
+         * to add private fields (e.g. head).
 
-     * As an example:
-     * class Garden {
-     *     private static class Potato {
-     *        int n;
-     *        public Potato(int nval) {
-     *           n = nval;
-     *        }
-     *     }
-     * }
-     * You are NOT required to do this, just an extra thing you can
-     * do if you want to avoid making a separate .java file. */
+         * But that's not all!
 
-}
+         * You will need to create at least one additional class for WeirdList
+         * to work. This is because you are forbidden to use any of the
+         * following in ANY of the code for HW3:
+         *       if, switch, while, for, do, try, or the ?: operator.
 
-/*
- * Hint: The first non-trivial thing you'll probably do to WeirdList
- * is to fix the EMPTY static variable so that it points at something
- * useful. */
+         * If you'd like an obtuse hint, scroll to the very bottom of this
+         * file.
+
+         * You can create this hypothetical class (or classes) in separate
+         * files like you usually do, or if you're feeling bold you can
+         * actually stick them INSIDE of this class. Yes, nested classes
+         * are a thing in Java.
+
+         * As an example:
+         * class Garden {
+         *     private static class Potato {
+         *        int n;
+         *        public Potato(int nval) {
+         *           n = nval;
+         *        }
+         *     }
+         * }
+         * You are NOT required to do this, just an extra thing you can
+         * do if you want to avoid making a separate .java file. */
+
+
+
+        /*
+         * Hint: The first non-trivial thing you'll probably do to WeirdList
+         * is to fix the EMPTY static variable so that it points at something
+         * useful. */
+    }
