928a82c5e74de7707b75e9858197488d202e1953
Tyler Rathkamp <tyrathkamp@berkeley.edu>
2020-02-21 17:37:37 -0800

    working

--- lab5/EqualityFilter.java
+++ lab5/EqualityFilter.java
@@ -4,16 +4,24 @@
  * @author Matthew Owen
  */
 public class EqualityFilter extends TableFilter {
-
+    public int index1;
+    public String mat;
+    public Table table;
     public EqualityFilter(Table input, String colName, String match) {
         super(input);
-        // FIXME: Add your code here.
-
+        index1 = input.colNameToIndex(colName);
+        table = input;
+        mat = match;
     }
 
     @Override
     protected boolean keep() {
-        // FIXME: Replace this line with your code.
+        Table.TableRow current = _next;
+        String one = new String (current.getValue(index1));
+        if (one.equals(mat)) {
+            return true;
+        }
+
         return false;
     }
 
