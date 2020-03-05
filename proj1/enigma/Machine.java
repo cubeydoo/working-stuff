aa10f9a20892b5fe2d4daf72b5509becc8e177f3
Tyler Rathkamp <tyrathkamp@berkeley.edu>
2020-03-04 22:31:27 -0800

    fix

--- proj1/enigma/Machine.java
+++ proj1/enigma/Machine.java
@@ -1,12 +1,13 @@
 package enigma;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Collection;
 
 import static enigma.EnigmaException.*;
 
 /** Class that represents a complete enigma machine.
- *  @author
+ *  @author Tyler Rathkamp
  */
 class Machine {
 
@@ -16,17 +17,19 @@ class Machine {
     Machine(Alphabet alpha, int numRotors, int pawls,
             Collection<Rotor> allRotors) {
         _alphabet = alpha;
-        // FIXME
+        _numRotors = numRotors;
+        _pawls = pawls;
+        _allRotors = allRotors;
     }
 
     /** Return the number of rotor slots I have. */
     int numRotors() {
-        return 0; // FIXME
+        return _numRotors;
     }
 
     /** Return the number pawls (and thus rotating rotors) I have. */
     int numPawls() {
-        return 0; // FIXME
+        return _pawls;
     }
 
     /** Set my rotor slots to the rotors named ROTORS from my set of
@@ -40,17 +43,28 @@ class Machine {
      *  numRotors()-1 characters in my alphabet. The first letter refers
      *  to the leftmost rotor setting (not counting the reflector).  */
     void setRotors(String setting) {
-        // FIXME
+        if (setting.length() != numRotors() - 1) {
+            throw new EnigmaException("Setting must include each rotor.");
+        } else {
+            for (int i = 0; i < numRotors(); i++) {
+                _rotors.get(i).set(setting.charAt(i));
+            }
+        }
     }
 
     /** Set the plugboard to PLUGBOARD. */
     void setPlugboard(Permutation plugboard) {
-        // FIXME
+        for (int i = 0; i < plugboard.size(); i++) {
+            if (plugboard.permute(plugboard.permute(i)) != i) {
+                throw new EnigmaException
+                        ("Plugboard must map each character to one other and vice versa.");
+            }
+        }
+        _permutation = plugboard;
     }
 
     /** Returns the result of converting the input character C (as an
      *  index in the range 0..alphabet size - 1), after first advancing
-
      *  the machine. */
     int convert(int c) {
         return 0; // FIXME
@@ -64,6 +78,9 @@ class Machine {
 
     /** Common alphabet of my rotors. */
     private final Alphabet _alphabet;
-
-    // FIXME: ADDITIONAL FIELDS HERE, IF NEEDED.
+    private int _numRotors;
+    private int _pawls;
+    private Permutation _permutation;
+    private Collection<Rotor> _allRotors;
+    private ArrayList<Rotor> _rotors;
 }
