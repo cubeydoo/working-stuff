989bca4f0bdb113c59716c4b0eaaca6809d84a06
Tyler Rathkamp <tyrathkamp@berkeley.edu>
2020-03-06 19:08:43 -0800

    p3

--- hw4/P2Pattern.java
+++ hw4/P2Pattern.java
@@ -14,7 +14,7 @@ public class P2Pattern {
     public static String P2 = "^\\((\\d+,+ *)* [0-9]*\\)$";
 
     /* Pattern to match a valid domain name. Eg: www.support.facebook-login.com */
-    public static String P3 = ""; 
+    public static String P3 = "^([a-z]|[0-9])(\\.|-)?(([a-z]|[0-9]|[A-Z])+(\\.|-))*([a-z]){2,6}";
 
     /* Pattern to match a valid java variable name. Eg: _child13$ */
     public static String P4 = ""; //FIXME: Add your regex here
