cfafe3dbcc8491196d25c4d13e950570a874c1e4
Tyler Rathkamp <tyrathkamp@berkeley.edu>
2020-05-01 00:27:12 -0700

    globalLog

--- proj3/gitlet/Main.java
+++ proj3/gitlet/Main.java
@@ -10,24 +10,30 @@ public class Main {
             new Commit("initial commit");
         } else if (!GITLET.exists()) {
             System.out.println("Not in an initialized Gitlet directory.");
-        } else if (args[0].equals("add")) {
-            Commands.addFile(args[1]);
-        } else if (args[0].equals("commit")) {
-            new Commit(args[1]);
-        } else if (args[0].equals("checkout")) {
-            if (args[1].equals("--")) {
-                Commands.checkout(null, args[2]);
-            } else if (args[1].equals("++")) {
-                System.out.println("Incorrect operands.");
-            } else if (args.length == 4) {
-                Commands.checkout(args[1], args[3]);
-            } else if (args.length == 2) {
-                Commands.checkout(args[1]);
-            }
-        } else if (args[0].equals("log")) {
-            Display.log();
-        } else {
-            System.out.println("I don't understand.");
+        }
+        switch (args[0]) {
+            case "add" :
+                Commands.addFile(args[1]);
+            case "commit":
+                new Commit(args[1]);
+            case "checkout":
+                if (args[1].equals("--")) {
+                    Commands.checkout(null, args[2]);
+                } else if (args[1].equals("++")) {
+                    System.out.println("Incorrect operands.");
+                } else if (args.length == 4) {
+                    Commands.checkout(args[1], args[3]);
+                } else if (args.length == 2) {
+                    Commands.checkout(args[1]);
+                }
+            case "log":
+                Display.log();
+            case "rm":
+                Commit.rm(args[1]);
+            case "global-log":
+                Display.globalLog();
+            default:
+                System.out.println("I don't understand.");
         }
     }
 }
